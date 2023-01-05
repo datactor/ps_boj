0a300722958d97fbf7b449ee0146c996a897d792
jam <90451102+datactor@users.noreply.github.com>
2023-01-05 16:40:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 16 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.rs
@@ -0,0 +1,38 @@
+use std::{
+    io::{self, prelude::*},
+    error::Error,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    let mut lines = input.lines();
+
+    let mut first_line = lines.next().unwrap().split_ascii_whitespace().map(
+        |s| s.parse::<usize>()).flatten();
+    let n = first_line.next().unwrap();
+    let k = first_line.next().unwrap();
+
+    let mut v: Vec<(usize, usize)> = lines.map(
+        |s| {
+            let mut a = s.split_ascii_whitespace()
+                .map(|y| y.parse::<usize>()).flatten();
+            (a.next().unwrap(), a.next().unwrap())
+        }
+    ).collect();
+
+    v.sort();
+
+    let mut arr = vec![0; k+1];
+
+    for &(w, v) in v.iter() {
+        for j in (1..k+1).rev() {
+            if j >= w {
+                arr[j] = arr[j].max(arr[j-w] + v);
+            }
+        }
+    }
+
+    println!("{}", arr[k]);
+    Ok(())
+}
