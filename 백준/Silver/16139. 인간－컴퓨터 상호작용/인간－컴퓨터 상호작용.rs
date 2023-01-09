92fe76b9a0629066c0281c4bb3b533115f1402ed
jam <90451102+datactor@users.noreply.github.com>
2023-01-09 17:54:33 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 84 ms, Memory: 37580KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.rs
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.rs
@@ -10,18 +10,31 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut lines = input.lines();
 
+
     let s = lines.next().unwrap().split_ascii_whitespace().map(|s| s.as_bytes()).collect::<Vec<_>>();
+
+    let mut arr = vec![[0; 26]; s[0].len()];
+    arr[0][s[0][0] as usize - 97] = 1;
+
+    for i in 1..s[0].len() {
+        let j =  (s[0][i] - 97) as usize;
+        arr[i] = arr[i-1];
+        arr[i][j] = arr[i-1][j] + 1;
+    }
+
 
     for line in lines.skip(1) {
         let mut v = line.split_ascii_whitespace().map(|s| s.parse::<String>()).flatten();
 
-        let a = v.next().unwrap().as_bytes()[0];
+        let a = (v.next().unwrap().as_bytes()[0] - 97) as usize;
         let l = v.next().unwrap().parse::<usize>().unwrap();
         let r = v.next().unwrap().parse::<usize>().unwrap();
 
-        let x = &s[0][l..r+1].iter().filter(|&s| s == &a).count();
-        writeln!(output, "{}", x)?;
+        writeln!(output, "{}", match l {
+            0 => arr[r][a],
+            _ => arr[r][a] - arr[l-1][a],
+        })?;
     }
-    
+
     Ok(())
 }
