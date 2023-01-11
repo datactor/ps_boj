91ff74b72b9608e6871305b32b12550d308c2f13
jam <90451102+datactor@users.noreply.github.com>
2023-01-11 17:13:41 +0900

    [Gold III] Title: 나머지 합, Time: 40 ms, Memory: 29544 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.rs
+++ 백준/Gold/10986. 나머지 합/나머지 합.rs
@@ -7,28 +7,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let mut lines = input.lines();
-
-    let mut first_line = lines.next().unwrap().split_ascii_whitespace().map(
+    let mut v = input.split_ascii_whitespace().map(
         |s| s.parse::<usize>()).flatten();
 
-    let n = first_line.next().unwrap();
-    let m = first_line.next().unwrap();
+    let n = v.next().unwrap();
+    let m = v.next().unwrap();
 
-    let mut arr: Vec<i64> = vec![0; m];
-    arr[0] = 1;
-
-    let v: Vec<usize> = lines.next().unwrap().split_ascii_whitespace().map(
-        |s| s.parse::<usize>().unwrap()).collect();
+    let mut res: Vec<i64> = vec![0; m];
 
     let mut total = 0;
     for i in 0..n {
-        total += v[i];
-        let r = total % m;
-        arr[r] += 1
+        total += v.next().unwrap();
+        res[total % m] += 1;
     }
 
-    println!("{}", (0..m).map(|i| arr[i] * (arr[i] - 1) / 2).sum::<i64>());
-
+    println!("{}", res[0] + (0..m).map(|i| res[i] * (res[i] - 1) / 2).sum::<i64>());
     Ok(())
 }
