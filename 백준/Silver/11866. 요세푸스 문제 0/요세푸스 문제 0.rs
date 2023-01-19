f95ddba4280b717667077effe5d550947223fb09
jam <90451102+datactor@users.noreply.github.com>
2023-01-19 19:55:06 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 4 ms, Memory: 13164 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.rs
@@ -0,0 +1,36 @@
+use std::{
+    io::{self, prelude::*, BufWriter},
+    error::Error,
+    collections::VecDeque,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    let mut output = BufWriter::new(io::stdout().lock());
+    io::stdin().read_line(&mut input)?;
+    write!(output, "<")?;
+
+    let mut v = input.split_ascii_whitespace();
+
+    let (n, k) =
+        (v.next().unwrap().parse::<usize>().unwrap(),
+         v.next().unwrap().parse::<usize>().unwrap());
+
+    let mut deck: VecDeque<usize> = (1..n+1).map(|s| s).collect();
+
+    let mut idx = k;
+    while deck.len() > 1 {
+        if idx <= deck.len() {
+            write!(output, "{}, ", deck[idx - 1]);
+            deck.remove(idx - 1);
+            idx += k - 1
+        } else {
+            while idx > deck.len() {
+                idx -= deck.len();
+            }
+        }
+    }
+
+    write!(output, "{}>", deck[0])?;
+    Ok(())
+}
