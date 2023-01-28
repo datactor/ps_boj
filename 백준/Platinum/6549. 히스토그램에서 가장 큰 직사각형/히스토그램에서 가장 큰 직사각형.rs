9ee70020f85c7b081f7e64be20167f288fe4a4f5
jam <90451102+datactor@users.noreply.github.com>
2023-01-28 12:38:41 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 12 ms, Memory: 18812 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.rs
@@ -0,0 +1,54 @@
+use std::{
+    io::{self, prelude::*, BufWriter},
+    error::Error,
+    collections::VecDeque,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    let mut output = BufWriter::new(io::stdout().lock());
+    io::stdin().read_to_string(&mut input)?;
+
+    let mut lines = input.lines();
+
+    loop {
+        let mut v = lines.next().unwrap().split_ascii_whitespace()
+            .map(|s| s.parse::<usize>()).flatten();
+        let n = v.next().unwrap() as usize;
+        if n == 0 {
+            break
+        }
+        let x: Vec<usize> = v.collect();
+        let mut answer = 0;
+
+        let mut stack = VecDeque::new();
+        for i in 0..n {
+            while !stack.is_empty() && x[*stack.back().unwrap() as usize] > x[i] {
+                let tmp = stack.pop_back().unwrap();
+                let mut width = 0;
+
+                if stack.is_empty() {
+                    width = i;
+                } else {
+                    width = i - stack.back().unwrap() - 1;
+                }
+
+                answer = usize::max(answer, width as usize * x[tmp]);
+            }
+            stack.push_back(i)
+        }
+        while !stack.is_empty() {
+            let tmp = stack.pop_back().unwrap();
+            let mut width = 0;
+            if stack.is_empty() {
+                width = n;
+            } else {
+                width = n - stack.back().unwrap() - 1;
+            } answer = usize::max(answer, width as usize * x[tmp])
+        }
+
+        writeln!(output, "{}", answer)?;
+    }
+
+    Ok(())
+}
