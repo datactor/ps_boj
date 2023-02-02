3a354862aa3e7395d06bc6ceec0102e715956a47
jam <90451102+datactor@users.noreply.github.com>
2023-02-02 12:38:00 +0900

    [Silver II] Title: 나무 자르기, Time: 52 ms, Memory: 33644 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.rs
@@ -0,0 +1,38 @@
+use std::{
+    io::{self, prelude::*},
+    error::Error,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let mut input = input.split_ascii_whitespace().skip(1).map(|s| s.parse::<i32>().unwrap());
+    let m = input.next().unwrap();
+    let mut v: Vec<i32> = input.collect();
+    
+    let (mut left, mut right) = (0, 999_999_999);
+    let mut mid = 0;
+
+    while left <= right {
+        let mut sum = 0;
+        mid = (left + right) / 2;
+        for i in &v {
+            if i - mid > 0 {
+                sum += i - mid;
+            }
+            if sum > 2_000_000_000 {
+                break
+            }
+        }
+        if sum >= m {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    println!("{}", right);
+
+    Ok(())
+}
