6ab0d127445886830100860e304df898e615f71f
jam <90451102+datactor@users.noreply.github.com>
2023-02-10 19:28:05 +0900

    Update 1로 만들기 2.rs
    [Silver I] Title: 1로 만들기 2, Time: 4 ms, Memory: 13172 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.rs
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.rs
@@ -1,57 +1,58 @@
+// https://www.acmicpc.net/problem/12852
+
 use std::{
     io::{self, prelude::*, BufWriter},
     error::Error,
     collections::HashMap,
 };
 
-struct Dp {
-    inner: HashMap<i32, (i32, Vec<i32>)>,
+struct MinSteps {
+    previous_steps: HashMap<i32, (i32, Vec<i32>)>,
 }
 
-impl Dp {
-    fn new() -> Dp {
-        Dp {
-            inner: HashMap::from([
+impl MinSteps {
+    fn new() -> MinSteps {
+        MinSteps {
+            previous_steps: HashMap::from([
                 (1, (0, vec![1])),
                 (2, (1, vec![1, 2])),
                 (3, (1, vec![1, 3])),
-            ]
-            )
+            ])
         }
     }
 
-    fn to_one(&mut self, n: i32) -> (i32, Vec<i32>) {
-        if let Some(inner) = self.inner.get(&n) {
-            return (*inner).clone();
+    fn get_steps_to_one(&mut self, num: i32) -> (i32, Vec<i32>) {
+        if let Some(prev_steps) = self.previous_steps.get(&num) {
+            return (*prev_steps).to_owned();
         }
 
-        let mut vec = Vec::new();
-        let (mut cnt, mut quo) = (0, Vec::new());
+        let mut possible_steps = Vec::new();
+        let (mut step_count, mut steps_sequence);
 
-        if n % 3 == 0 {
-            (cnt, quo) = self.to_one(n / 3);
-            vec.push((cnt + 1, quo));
+        if num % 3 == 0 {
+            (step_count, steps_sequence) = self.get_steps_to_one(num / 3);
+            possible_steps.push((step_count + 1, steps_sequence));
         }
-        if n % 2 == 0 {
-            (cnt, quo) = self.to_one(n / 2);
-            vec.push((cnt + 1, quo));
+        if num % 2 == 0 {
+            (step_count, steps_sequence) = self.get_steps_to_one(num / 2);
+            possible_steps.push((step_count + 1, steps_sequence));
         }
-        if (n - 1) % 3 == 0 || (n - 1) % 2 == 0 {
-            (cnt, quo) = self.to_one(n - 1);
-            vec.push((cnt + 1, quo));
+        if (num - 1) % 3 == 0 || (num - 1) % 2 == 0 {
+            (step_count, steps_sequence) = self.get_steps_to_one(num - 1);
+            possible_steps.push((step_count + 1, steps_sequence));
         }
-        if (n - 2) % 3 == 0 {
-            (cnt, quo) = self.to_one(n - 2);
-            quo.push(n - 1);
-            vec.push((cnt + 2, quo));
+        if (num - 2) % 3 == 0 {
+            (step_count, steps_sequence) = self.get_steps_to_one(num - 2);
+            steps_sequence.push(num - 1);
+            possible_steps.push((step_count + 2, steps_sequence));
         }
 
-        let (count, mut quo) = vec.iter().min().unwrap().clone();
+        let (step_count, mut steps_sequence) = possible_steps.iter().min().unwrap().to_owned();
 
-        quo.push(n);
-        self.inner.insert(n, (count, quo.clone()));
+        steps_sequence.push(num);
+        self.previous_steps.insert(num, (step_count, steps_sequence.to_owned()));
 
-        (count, quo)
+        (step_count, steps_sequence)
     }
 }
 
@@ -60,14 +61,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut output = BufWriter::new(io::stdout().lock());
     io::stdin().read_line(&mut input)?;
 
-    let n: i32 = input.trim().parse()?;
-    let mut dp = Dp::new();
+    let num: i32 = input.trim().parse()?;
+    let mut min_steps = MinSteps::new();
 
-    let (cnt, quo) = dp.to_one(n);
+    let (step_count, steps_sequence) = min_steps.get_steps_to_one(num);
 
-    writeln!(output, "{}", cnt)?;
-    for p in quo.iter().rev() {
-        write!(output, "{} ", p)?;
+    writeln!(output, "{}", step_count)?;
+    for step in steps_sequence.iter().rev() {
+        write!(output, "{} ", step)?;
     }
 
     Ok(())
