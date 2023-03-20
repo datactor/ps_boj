06b53c8675b935d536b8f08fdc194788431f02bb
jam <90451102+datactor@users.noreply.github.com>
2023-03-20 13:33:52 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 32 ms, Memory: 25452 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.rs
@@ -0,0 +1,42 @@
+use std::io::{self, prelude::*, BufWriter};
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    let mut output = BufWriter::new(io::stdout().lock());
+    io::stdin().lock().read_to_string(&mut input)?;
+
+    let heights: Vec<usize> = input
+        .split_ascii_whitespace()
+        .skip(1)
+        .map(|p| p.parse::<usize>().unwrap())
+        .collect();
+
+    let mut stack = Vec::new();
+    let mut ans = 0;
+
+    for h in heights {
+        while let Some((top, cnt)) = stack.pop() {
+            if top < h {
+                ans += cnt as u64;
+            } else {
+                stack.push((top, cnt));
+                break;
+            }
+        }
+        let mut cnt = 1;
+        if let Some((top, top_cnt)) = stack.last_mut() {
+            if *top == h {
+                cnt += *top_cnt;
+                ans += *top_cnt as u64;
+                stack.pop();
+            }
+        }
+        if !stack.is_empty() {
+            ans += 1;
+        }
+        stack.push((h, cnt));
+    }
+
+    writeln!(output, "{}", ans)?;
+    Ok(())
+}
